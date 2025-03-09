74b322a5c2a9679dcf1493dc4bc61a23ad26ed1e
SarojShresthaa <saroj@usome.com>
2025-03-09 21:37:08 +0545

    Update razor

---
+++ CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>  options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 1, Name = "Web Development", DisplayOrder = 1 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 2, Name = "Mobile Development", DisplayOrder = 2 });
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 3, Name = "Game Development", DisplayOrder = 3 });
+        }
+
+
+
+
+    }
+}
---
+++ CRUD/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Category
+    {
+        [Key] // Primary Key
+        public int Id { get; set; }
+        [Required] // Required field
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")] // Display name for the field
+        [Range(1,100, ErrorMessage ="Display Order Must be between, 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
