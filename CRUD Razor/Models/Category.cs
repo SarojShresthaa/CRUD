3938d4f8e34222b692bed2bbd704ad1efbe3dde1
SarojShresthaa <saroj@usome.com>
2025-03-09 22:17:33 +0545

    Update

---
+++ CRUD Razor/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CRUD_Razor.Models
+{
+    public class Category
+    {
+        [Key] // Primary Key
+        public int Id { get; set; }
+        [Required] // Required field
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")] // Display name for the field
+        [Range(1, 100, ErrorMessage = "Display Order Must be between, 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ CRUD Razor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,22 @@
+using CRUD_Razor.Data;
+using CRUD_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Razor.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
